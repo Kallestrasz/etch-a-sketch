ea3b4205f48d6b0993ea53a07b9f8ede86e4f190
Kallestarsz <skhunters14@gmail.com>
2022-05-29 22:35:37 +0000

    Add eraser and rainbow button events

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let grid = document.querySelector(".pad");
 let mouseDown = false;
 let gridSize = 16;
+let mod = 0;
 
 document.addEventListener("mousedown", () => {
   mouseDown = true;
@@ -19,7 +20,9 @@ function gridGen() {
       cell.addEventListener(
         "mouseover", function () {
           if (!mouseDown) return;
-          cell.style.background = "#3C9EE7";
+          if (mod == 0) cell.style.background = "#3C9EE7";
+          else if (mod == 1) cell.style.background = "whitesmoke";
+          else if (mod == 2) cell.style.background = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
         });
       grid.appendChild(cell);
     }
@@ -30,18 +33,11 @@ document.querySelector(".newGrid").addEventListener('click', () => {
   do gridSize = prompt("Enter new grid size"); while (isNaN(gridSize));
   if (grid) grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
   while (grid.firstElementChild) grid.firstElementChild.remove();
+  mod = 1;
   gridGen(gridSize);
 });
 
-// normal.addEventListener('click', () => {
-//   console.log("A");
-// });
-
-// NewGrid.addEventListener('click', () => {
-//   console.log("A");
-// });
-
-// newGrid.addEventListener('click', () => {
-//   console.log("A");
-// });
+document.querySelector(".default").addEventListener('click', () => { mod = 0 });
+document.querySelector(".eraser").addEventListener('click', () => { mod = 1 });
+document.querySelector(".rainbow").addEventListener('click', () => { mod = 2 });
 gridGen();
